4341c09b583087bbaca7557045edc0897dfcb583
Junior Alves <jjalves96@gmail.com>
2021-11-21 09:44:38 -0300

    feat: create Dropdown component

---
+++ src/components/Dropdown/stories.tsx
@@ -0,0 +1,20 @@
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Dropdown, { DropdownProps } from '.';
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta;
+
+export const Default: Story<DropdownProps> = (args) => <Dropdown {...args} />;
+
+Default.args = {
+  title: 'Click here',
+  children: 'content'
+};
---
+++ src/components/Dropdown/styles.ts
@@ -0,0 +1,79 @@
+import styled, { css } from 'styled-components';
+
+export const Title = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.white};
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-right: 2.4rem;
+    z-index: ${theme.layers.alwaysOnTop};
+  `}
+`;
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    background: ${theme.colors.white};
+    color: ${theme.colors.black};
+    margin-top: ${theme.spacings.small};
+    position: absolute;
+    right: 0;
+    z-index: ${theme.layers.alwaysOnTop};
+    box-shadow: 0 6px 8px rgba(0, 0, 0, 0.1);
+
+    &::before {
+      content: '';
+      position: absolute;
+      border-right: 1.2rem solid transparent;
+      border-left: 1.2rem solid transparent;
+      border-bottom: 1.2rem solid ${theme.colors.white};
+      top: -1.2rem;
+      right: 2.4rem;
+    }
+  `}
+`;
+
+export const Overlay = styled.div`
+  ${({ theme }) => css`
+    background: rgba(0, 0, 0, 0.2);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: ${theme.layers.overlay};
+  `}
+`;
+
+type ContainerProps = {
+  isOpen?: boolean;
+};
+
+const containerModifiers = {
+  open: () => css`
+    opacity: 1;
+    pointer-events: auto;
+    transform: translateY(0);
+  `,
+  close: () => css`
+    opacity: 0;
+    pointer-events: none;
+    transform: translateY(-2rem);
+  `
+};
+
+export const Container = styled.div<ContainerProps>`
+  ${({ theme, isOpen }) => css`
+    position: relative;
+    width: max-content;
+    ${Content},
+    ${Overlay} {
+      transition: transform 0.2s ease-in, opacity ${theme.transition.default};
+      ${isOpen && containerModifiers.open()}
+      ${!isOpen && containerModifiers.close()}
+    }
+  `}
+`;
