8287651dda23a635c5e12d63ecc03cad8673eb37
Junior Alves <jjalves96@gmail.com>
2021-11-21 20:17:29 -0300

    fix: fix lint styles

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,5 +1,3 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-export const Container = styled.div`
-  ${({ theme }) => css``}
-`;
+export const Container = styled.div``;
