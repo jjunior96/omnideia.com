5e2f800ebafc37f2349098fec4123be5e13b2c90
Junior Alves <jjalves96@gmail.com>
2021-11-21 09:50:06 -0300

    feat: create snapshot tests

---
+++ src/components/Header/data.ts
@@ -0,0 +1,29 @@
+const HeaderData = [
+  {
+    id: '1',
+    name: 'Home',
+    path: '/'
+  },
+  {
+    id: '2',
+    name: 'Services',
+    path: '/services'
+  },
+  {
+    id: '3',
+    name: 'Projects',
+    path: '/projects'
+  },
+  {
+    id: '4',
+    name: 'Contact',
+    path: '/contact'
+  },
+  {
+    id: '5',
+    name: 'About',
+    path: '/about'
+  }
+];
+
+export default HeaderData;
