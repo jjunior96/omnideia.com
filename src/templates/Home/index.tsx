8508ceaa18e1885431fa6a0cd01b362afc8ca10a
JUNIOR ALVES <jjalves96@gmail.com>
2021-11-22 08:06:29 -0300

    chore: add new components into Home page

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,4 +1,12 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { BsArrowRight as ArrowRightIcon } from 'react-icons/bs';
+
+import Button from 'components/Button';
+import Container from 'components/Container';
 import Header from 'components/Header';
+import Heading from 'components/Heading';
 
 import * as S from './styles';
 
@@ -6,6 +14,24 @@ const Home = () => {
   return (
     <S.Container>
       <Header />
+
+      <Container>
+        <S.SectionContainer>
+          <S.SectionInfo>
+            <Heading size="huge">A digital Product design agency</Heading>
+
+            <Link href="/services" passHref>
+              <Button as="a" icon={<ArrowRightIcon />} positionIcon="right">
+                Our Services
+              </Button>
+            </Link>
+          </S.SectionInfo>
+
+          <S.SectionImage>
+            <Image src="/img/image-people.png" width="1000" height="1000" />
+          </S.SectionImage>
+        </S.SectionContainer>
+      </Container>
     </S.Container>
   );
 };
--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -1,3 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import * as ButtonStyles from 'components/Button/styles';
 
 export const Container = styled.div``;
+
+export const SectionContainer = styled.section`
+  ${({ theme }) => css`
+    margin-top: ${theme.grid.header};
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    align-items: center;
+  `}
+`;
+
+export const SectionInfo = styled.div`
+  ${({ theme }) => css`
+    ${ButtonStyles.Container} {
+      width: 50%;
+
+      margin-top: ${theme.spacings.medium};
+    }
+  `};
+`;
+
+export const SectionImage = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
