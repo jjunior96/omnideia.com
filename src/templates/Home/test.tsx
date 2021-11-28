7c53c48ee504fae1ba960432de467bb235493c60
Junior Alves <jjalves96@gmail.com>
2021-11-28 07:38:54 -0300

    test: add Base template on Home page

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -14,11 +14,39 @@ jest.mock('components/Header', () => {
   };
 });
 
+// Mock para o componente Footer
+jest.mock('components/Footer', () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Footer"></div>;
+    }
+  };
+});
+
 describe('<Home />', () => {
   it('should render the Home page', () => {
     renderWithTheme(<Home />);
 
     // Verifica se o Mock do Header foi chamado
     expect(screen.getByTestId('Mock Header')).toBeInTheDocument();
+
+    // Verifica se o Heading da pagina foi renderizado
+    expect(
+      screen.getByRole('heading', { name: /A digital Product design agency/i })
+    ).toBeInTheDocument();
+
+    // Verifica se o Button (as Link) da pagina foi renderizado
+    expect(
+      screen.getByRole('link', { name: /Our Services/i })
+    ).toBeInTheDocument();
+
+    // Verifica se a Image da pagina foi renderizada
+    expect(
+      screen.getByRole('img', { name: /people together/i })
+    ).toBeInTheDocument();
+
+    // Verifica se o Mock do Footer foi chamado
+    expect(screen.getByTestId('Mock Footer')).toBeInTheDocument();
   });
 });
