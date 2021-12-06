2eef44d4992117ac8161c544270b14f0a1fdde14
Junior Alves <jjalves96@gmail.com>
2021-12-06 08:30:40 -0300

    chore: change name page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,11 +11,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>App Name</title>
+        <title>Omnideia</title>
         <link rel="shorcut icon" href="/img/bg.png" />
         <link rel="apple-touch-icon" href="/img/bg.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="App Name" />
+        <meta name="description" content="Omnideia" />
       </Head>
 
       <AppProvider>
